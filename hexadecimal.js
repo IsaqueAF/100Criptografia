4201b866730c0ccadff94d961d55a0f40864887b
Vladimir-Ramalho <vladimirramalho7@gmail.com>
2026-08-27 18:55:22 -0300

    Add files via upload

---
+++ hexadecimal.js
@@ -0,0 +1,42 @@
+function codificacao(texto) {   
+    let resultado = "";
+
+    for (let i = 0; i < texto.length; i++) {
+        
+        let caractere = texto[i];
+        let codigo = caractere.charCodeAt(0);
+        if (codigo >= 97 && codigo <= 122) {
+            let hexa = codigo.toString(16);
+            resultado += hexa + " ";
+        } else if (codigo >= 65 && codigo <= 90) {
+            let hexa = codigo.toString(16);
+            resultado += hexa + " ";
+        } else if (codigo === 32) {
+            let hexa = codigo.toString(16);
+            resultado += hexa + " ";
+        }
+    }
+
+    return resultado;
+
+}
+
+
+function decodificacao(digito) {   
+    let resultado = "";
+
+    let blocoAscii = digito.split(" ");
+
+    for (let i = 0; i < blocoAscii.length; i ++) {
+        let vireInteiro = parseInt(blocoAscii[i], 16);
+
+        if (isNaN(vireInteiro)) continue;
+
+        const converter = String.fromCharCode(vireInteiro);
+
+        resultado += converter;
+    }
+
+    return resultado;
+
+}
